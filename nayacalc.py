c08682e1fc156d25f134c3410ff8b1ca7aff1be6
beginnerpyth <hskx7779@gmail.com>
2026-01-02 23:52:24 +0900

    Add files via upload

---
+++ nayacalc.py
@@ -0,0 +1,14 @@
+num1=float(input("enter a numbeer one"))
+num2 = float(input("enter a number two"))
+#mul= int(input("enter a number"))
+#div=int(input("enter a number"))
+op = input("enter a opertaor")
+if op == "+" :
+    print(num1+num2)
+elif op == "-" :
+    print(num1-num2)
+elif op == "/" :
+    print(num1/num2)
+else:
+    print("click some operator")
+
---
+++ nestedloop.py
@@ -0,0 +1,17 @@
+for x in range(5):
+    for c in range(x):
+        print(c+1,end="")
+    print()
+
+    #for d in range(9):
+     #   for m in range(d):
+     #       print(m+1,end="")
+      #  print()
+      # 
+      # 
+#m = " hello string" 
+#for x in range(len(m)):
+ #           print(m[x])
+b = ("IMAAMANOFHOUSE")
+print(b[2].lower())
+
